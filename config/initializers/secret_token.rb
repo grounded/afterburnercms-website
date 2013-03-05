e76e2bf52a0797f66ee5b4b8c5bff74cf57238aa
Uģis Ozols <ugis.ozolss@gmail.com>
2013-03-05 11:45:00 +0200

    Specify Ruby version in Gemfile to make Heroku happy.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Afterburnercms::Application.config.secret_key_base = 'dd2015496af0cc3561b6130d0adb182fde20fd4484e8132f16b69afa33c93217409ca98715114a7e6cd34aacca601cd25f148ce9ce64fbc412a5d75bc717a176'
+Afterburnercms::Application.config.secret_key_base = 'dd3015496af0cc3561b6130d0adb182fde20fd4484e8132f16b69afa33c93217409ca98715114a7e6cd34aacca601cd25f148ce9ce64fbc412a5d75bc717a176'
