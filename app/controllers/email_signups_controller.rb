ca69a06accc0cc05107c93630f59a840ba0e8fd0
Philip Arndt <parndt@gmail.com>
2013-03-18 09:36:10 +1300

    Added page presenter though we're not requiring delivery/frontend so it does not work.

--- app/controllers/email_signups_controller.rb
+++ app/controllers/email_signups_controller.rb
@@ -1,6 +1,7 @@
 require 'ostruct'
-# require 'app/conductors/shows_email_signup_form'
-# require 'app/conductors/accepts_email_signup_form'
+require_relative '../conductors/shows_email_signup_form'
+require_relative '../conductors/accepts_email_signup_form'
+require 'abc/html/page_presenter'
 
 class EmailSignupsController < ApplicationController
   def new
@@ -16,4 +17,10 @@ class EmailSignupsController < ApplicationController
 
     render :template => 'email_signups/thank_you'
   end
+
+  protected
+  def page_presenter
+    Abc::Html::PagePresenter.new @data.page
+  end
+  helper_method :page_presenter
 end
