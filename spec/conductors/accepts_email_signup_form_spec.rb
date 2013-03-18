74cb43b72afd637f0706f1ab754e74146504cc35
Rob Yurkowski <rob@yurkowski.net>
2013-03-17 22:52:03 -0400

    Add enhanced responsibilities to email signup acceptance conductor.

--- spec/conductors/accepts_email_signup_form_spec.rb
+++ spec/conductors/accepts_email_signup_form_spec.rb
@@ -1,6 +1,6 @@
 require 'tiny_spec_helper'
 
-module Abc; 
+module Abc
   class BaseConductor
     def call(params, opts = {}); new.to_response; end;
   end
@@ -20,5 +20,34 @@ describe AcceptsEmailSignupForm do
 
   it "returns a hash containing an email signup" do
     result[:email_signup].should respond_to(:email)
+  end
+
+  it "hands off its form data to the interactor" do
+    pending
+  end
+
+  context "when validations fail" do
+    it "should set errors" do
+      pending
+    end
+  end
+
+  context "when validations are okay" do
+    it "stores the data" do
+      pending
+    end
+  end
+
+  context "when storing is okay" do
+    it "passes data back to the conductor" do
+      # What is the output of subject.call? Is it expected?
+      pending
+    end
+  end
+
+  context "when storing isn't okay" do
+    it "raises an error" do
+      pending
+    end
   end
 end
